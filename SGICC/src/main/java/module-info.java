dda7c953384ab19f3b37dac1bf131268cfcbfb35
Melchor22 <zs20015751@estudiantes.uv.mx>
2023-10-11 17:07:27 -0600

    Actualización de Proyecto

--- SGICC/src/main/java/module-info.java
+++ SGICC/src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.example.sgicc {
     requires javafx.fxml;
 
     requires com.dlsc.formsfx;
+    requires java.sql;
 
     opens com.example.sgicc to javafx.fxml;
     exports com.example.sgicc;
